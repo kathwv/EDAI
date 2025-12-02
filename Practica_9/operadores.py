3f893246e4cbc3280a955a19b3285aa5ae78701f
kathwv <kaathwv@gmail.com>
2025-12-01 22:01:20 -0600

    operadores.py

---
+++ Practica_9/operadores.py
@@ -0,0 +1,15 @@
+#para el exponente se puede utilizar asaterisco
+print( 1 + 5 )
+print( 6 * 3 )
+print( 10 - 4 )
+print( 100 / 50 )
+print( 10 % 2 )
+print( ((20 * 3) + (10 + 1)) / 10)
+print( 2**2 )
+
+False and True
+print (7 < 5) 
+print (7 > 5)
+print ((11 * 3)+2 == 36 - 1)
+print ((11 * 3)+2 >= 36) 
+print ("curso" != "CuRsO") 
